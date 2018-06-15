722e6a3635ce48269e799f067a98b328e1ce2b89
Pramit Biswas <pramitbiswas@users.noreply.github.com>
2018-06-15 16:44:20 +0530

    Add files via upload
    Language-specific *.js loaded only once. updating according to the selected language.

--- multiLanguage/js/language/lang.js
+++ multiLanguage/js/language/lang.js
@@ -1,27 +1,36 @@
+var langJsFileLoadFlag = {
+	"en":false,
+	"bn":false
+};
 function initLangFunc() {
 	langSelected = 'en';
 	loadLangScript(langSelected, getLangDependentValues);
 }
 window.addEventListener("DOMContentLoaded", function() {
     document.getElementById("LangMenu").onchange = function(){
-	langSelected = this.value;
-	loadLangScript(langSelected, getLangDependentValues);
+		langSelected = this.value;
+		if (langJsFileLoadFlag[langSelected] == false) {
+			loadLangScript(langSelected, getLangDependentValues);
+		} else {
+			getLangDependentValues();
+		}
 	};
 }, false);
 function loadLangScript(langSelected, callback)
-{
+{	
 	// Adding the script tag to the head as suggested before
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'js/language/' + langSelected + '.js';
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'js/language/' + langSelected + '.js';
 	script.id = langSelected;
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
+	// Then bind the event to the callback function.
+	// There are several events for cross browser compatibility.
 	script.onreadystatechange = callback;
 	script.onload = callback;
-    // Fire the loading
-    head.appendChild(script);
+	// Fire the loading
+	head.appendChild(script);
+	langJsFileLoadFlag[langSelected] = true;
 }
 var getLangDependentValues = function() {
    // GetValues   
